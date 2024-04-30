c9e37b98796b3c7209c03b4a24b4e280b55d9b9f
NjoguRufus <rufus.njogu@student.moringaschool.com>
2024-04-30 14:33:56 +0300

    react hooks props

---
+++ images/src/data/user.js
@@ -0,0 +1,12 @@
+const user = {
+  name: "Ian",
+  city: "Nairobi",
+  bio: "I made this!",
+  color: "firebrick",
+  links: {
+    github: "https://github.com/IanMwangiii",
+    linkedin: "https://www.linkedin.com/in/ian-mwangi-7a4394306/?lipi=urn%3Ali%3Apage%3Afeed_index_index%3B822abefb-6040-4474-b6a8-a5611744e8e9",
+  },
+};
+
+export default user;
